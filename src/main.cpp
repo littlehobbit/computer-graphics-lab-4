7eaef6e3eb7d3956f6dfc97db63a79f32800bcae
little_hobbit <rusikarm3@gmail.com>
2022-12-07 18:26:03 +0300

    bugged

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,343 @@
-#include <iostream>
+#include <X11/Xprotostr.h>
 
-int main(int argc, char *argv[]) {
-    std::cout << "Hello, world!" << std::endl;
+#include <SFML/Graphics.hpp>
+#include <algorithm>
+#include <cmath>
+#include <initializer_list>
+#include <iostream>
+#include <list>
+#include <optional>
+#include <set>
+#include <tuple>
+#include <utility>
+#include <vector>
+
+#include "SFML/Graphics/Color.hpp"
+#include "SFML/Graphics/PrimitiveType.hpp"
+#include "SFML/Graphics/RenderWindow.hpp"
+#include "SFML/Graphics/Vertex.hpp"
+#include "SFML/Graphics/VertexArray.hpp"
+#include "SFML/System/Vector2.hpp"
+#include "SFML/Window/VideoMode.hpp"
+
+using Vector2d = sf::Vector2<double>;
+using Point = sf::Vector2<double>;
+
+constexpr float eps = 0.001;
+namespace std {
+bool operator<(const Point& lhs, const Point& rhs) {
+  return lhs.x < rhs.x || lhs.x == rhs.x && lhs.y < rhs.y;
+}
+}  // namespace std
+
+// bool operator==(const Point& lhs, const Point& rhs) {
+//   return std::abs(lhs.x - rhs.x) <= eps && std::abs(lhs.y - rhs.y) <= eps;
+// }
+
+// bool operator!=(const Point& lhs, const Point& rhs) { return !(lhs == rhs); }
+
+struct Line {
+  Point start;
+  Point end;
+};
+
+class Shape {
+ public:
+  Shape(std::list<Point> points) : _points(std::move(points)) {
+    auto edge_start = _points.end(), edge_end = _points.begin();
+    edge_start--;
+    while (edge_end != _points.end()) {
+      _edges.push_back({*edge_start, *edge_end});
+      edge_start = edge_end++;
+    }
+  }
+
+  Shape(const std::initializer_list<Point>& points) : _points(points) {
+    auto edge_start = _points.end(), edge_end = _points.begin();
+    edge_start--;
+    while (edge_end != _points.end()) {
+      _edges.push_back({*edge_start, *edge_end});
+      edge_start = edge_end++;
+    }
+  }
+
+  const auto& get_edges() const { return _edges; }
+
+  const auto& get_points() const { return _points; }
+
+ private:
+  std::list<Point> _points;
+  std::list<Line> _edges;
+};
+
+enum class IntersectType { In, Out, Parallel };
+
+using IntersectPoint = std::pair<Point, IntersectType>;
+
+double len(const Vector2d& vec) {
+  return std::sqrt(vec.x * vec.x + vec.y * vec.y);
+}
+
+Vector2d get_in_normal(const Line& line) {
+  return Vector2d{line.end.y - line.start.y, -(line.end.x - line.start.x)};
+}
+
+double dot_product(const Vector2d& lhs, const Vector2d& rhs) {
+  return lhs.x * rhs.x + lhs.y * rhs.y;
+}
+
+std::optional<Point> intersect(const Line& lhs, const Line& rhs) {
+  auto normal = get_in_normal(rhs);
+
+  auto upper = dot_product(normal, lhs.start - rhs.start);
+  auto bottom = dot_product(normal, lhs.end - lhs.start);
+
+  if (bottom == 0) {
+    return {};
+  }
+
+  auto t = -upper / bottom;
+
+  if (t < 0 || t > 1) {
+    return {};
+  }
+
+  auto p_t = [&lhs](auto t) { return lhs.start + (lhs.end - lhs.start) * t; };
+
+  return p_t(t);
+}
+
+// Cyrus–Beck
+std::optional<IntersectPoint> get_intersection(const Line& line,
+                                               const Line& edge) {
+  auto normal = get_in_normal(edge);
+
+  auto intersection = intersect(line, edge);
+
+  if (!intersection || !intersect(edge, line)) {
+    return {};
+  }
+
+  auto product = dot_product(normal, line.end - edge.start);
+
+  if (product > 0) {
+    return IntersectPoint{*intersection, IntersectType::Out};
+  } else if (product < 0) {
+    return IntersectPoint{*intersection, IntersectType::In};
+  } else {
+    return IntersectPoint{*intersection, IntersectType::Parallel};
+  }
+
+  return {};
+}
+
+void insert_by_line(const Point& point, const Line& line,
+                    std::list<Point>& list) {
+  if (std::find(list.begin(), list.end(), point) != list.end()) return;
+
+  auto begin = std::find(list.begin(), list.end(), line.start);
+  auto end = std::find(list.begin(), list.end(), line.end);
+
+  const auto distance_from_start = len(point - line.start);
+  auto it = begin;
+  it++;
+
+  while (it != end && it != list.end()) {
+    if (len(*it - *begin) >= distance_from_start) {
+      break;
+    }
+    it++;
+  }
+
+  list.insert(it, point);
+}
+
+std::list<Shape> get_masked_shapes(std::list<Point> clipped,
+                                   std::list<Point> clipping,
+                                   std::set<Point> in_points,
+                                   std::set<Point> out_points) {
+  std::set<Point> used;
+  std::list<Shape> masked_shapes;
+  // return  masked_shapes;
+
+  for (const auto& point : in_points) {
+    if (used.count(point)) {
+      continue;
+    }
+
+    const auto start_point = point;
+    std::list<Point> masked{};
+
+    auto total = std::find(clipped.begin(), clipped.end(), start_point);
+
+    do {
+      used.insert(*total);
+
+      while (out_points.count(*total) == 0) {
+        used.insert(*total);
+        masked.push_back(*total);
+
+        total++;
+        if (total == clipped.end()) {
+          total = clipped.begin();
+        }
+      }
+
+      total = std::find(clipping.begin(), clipping.end(), *total);
+
+      while (in_points.count(*total) == 0) {
+        used.insert(*total);
+        masked.push_back(*total);
+
+        total++;
+        if (total == clipping.end()) {
+          total = clipping.begin();
+        }
+      }
+
+      total = std::find(clipped.begin(), clipped.end(), *total);
+
+    } while (*total != start_point);
+
+    masked_shapes.emplace_back(std::move(masked));
+  }
+
+  return masked_shapes;
+}
+
+std::list<Shape> get_merged_shapes(std::list<Point> clipped,
+                                   std::list<Point> clipping,
+                                   std::set<Point> in_points,
+                                   std::set<Point> out_points) {
+  std::set<Point> used;
+  std::list<Shape> merged_shapes;
+  // return  merged_shapes
+
+  for (const auto& point : out_points) {
+    if (used.count(point)) {
+      continue;
+    }
+
+    const auto start_point = point;
+    std::list<Point> merged{};
+
+    auto total = std::find(clipped.begin(), clipped.end(), start_point);
+
+    do {
+      while (in_points.count(*total) == 0) {
+        used.insert(*total);
+        merged.push_back(*total);
+
+        total++;
+        if (total == clipped.end()) {
+          total = clipped.begin();
+        }
+      }
+
+      auto reversed_total =
+          std::find(clipping.rbegin(), clipping.rend(), *total);
+
+      while (out_points.count(*reversed_total) == 0) {
+        used.insert(*reversed_total);
+        merged.push_back(*reversed_total);
+
+        reversed_total++;
+        if (reversed_total == clipping.rend()) {
+          reversed_total = clipping.rbegin();
+        }
+      }
+
+      total = std::find(clipped.begin(), clipped.end(), *reversed_total);
+
+    } while (*total != start_point);
+
+    merged_shapes.emplace_back(std::move(merged));
+  }
+
+  return merged_shapes;
+}
+
+auto wailer_atherton_clipping(const Shape& clipped, const Shape& clipping) {
+  std::set<Point> in_points, out_points;
+
+  auto clipped_points = clipped.get_points();
+  auto clipping_points = clipping.get_points();
+
+  for (const auto& line : clipped.get_edges()) {
+    for (const auto& edge : clipping.get_edges()) {
+      auto intersection = get_intersection(line, edge);
+
+      if (intersection.has_value()) {
+        if (intersection->second == IntersectType::In) {
+          in_points.insert(intersection->first);
+        } else {
+          out_points.insert(intersection->first);
+        }
+
+        insert_by_line(intersection->first, line, clipped_points);
+        insert_by_line(intersection->first, edge, clipping_points);
+      }
+    }
+  }
+
+  auto merged_shapes =
+      get_merged_shapes(clipped_points, clipping_points, in_points, out_points);
+  auto masked_shapes =
+      get_masked_shapes(clipped_points, clipping_points, in_points, out_points);
+
+  return std::make_pair(std::move(merged_shapes), std::move(masked_shapes));
+}
+
+int main(int argc, char* argv[]) {
+  Shape triangle = {{200, 550}, {400, 200}, {600, 550}};
+
+  const auto square_side = 350;
+  float start_x = 50;
+  float step = 0;
+
+  sf::RenderWindow window(sf::VideoMode(800, 800), "lab-4");
+
+  while (window.isOpen()) {
+    sf::Event event;
+    while (window.pollEvent(event)) {
+      if (event.type == sf::Event::Closed) {
+        window.close();
+      }
+    }
+
+    window.clear();
+
+    Shape square = {{start_x, 200},
+                    {start_x + square_side, 200},
+                    {start_x + square_side, 200 + square_side},
+                    {start_x, 200 + square_side}};
+
+    for (const auto& [start, end] : square.get_edges()) {
+      sf::Vertex vertex[] = {sf::Vector2f(start.x, start.y),
+                             sf::Vector2f(end.x, end.y)};
+      window.draw(vertex, 2, sf::PrimitiveType::Lines);
+    }
+
+    auto [merged, masked] = wailer_atherton_clipping(square, triangle);
+    for (const auto& shape : masked) {
+      for (const auto& [start, end] : shape.get_edges()) {
+        sf::Vertex vertex[] = {{sf::Vector2f(start.x, start.y), sf::Color::Red},
+                               {sf::Vector2f(end.x, end.y), sf::Color::Red}};
+        window.draw(vertex, 2, sf::PrimitiveType::Lines);
+      }
+    }
+
+    for (const auto& [start, end] : triangle.get_edges()) {
+      sf::Vertex vertex[] = {sf::Vector2f(start.x, start.y),
+                             sf::Vector2f(end.x, end.y)};
+      window.draw(vertex, 2, sf::PrimitiveType::Lines);
+    }
+
+    start_x += step;
+    if (start_x + square_side > window.getSize().x || start_x <= 0) {
+      step *= -1;
+    }
+
+    window.display();
+  }
 }
